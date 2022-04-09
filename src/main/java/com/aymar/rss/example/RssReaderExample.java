e0f7946e26c69f7a34bf441b7ee91c3bf5c1b379
aymar99 <lramya26031999@gmail.com>
2022-04-09 17:17:00 +0530

    Add functionality to read mulitple url rss feed

--- src/main/java/com/aymar/rss/example/RssReaderExample.java
+++ src/main/java/com/aymar/rss/example/RssReaderExample.java
@@ -1,6 +1,8 @@
 package com.aymar.rss.example;
 
 import com.aymar.rss.parser.RssFeedParser;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.log4j.BasicConfigurator;
 
@@ -9,6 +11,11 @@ public class RssReaderExample {
   public static void main(String[] args) {
     BasicConfigurator.configure();
     RssFeedParser parser = new RssFeedParser();
-    log.info("Feed list size {}", parser.parseRssFeedItems("https://den.dev/index.xml").size());
+    log.info(
+        "Feed list size {}", parser.parseRssFeedItemFromURL("https://den.dev/index.xml").size());
+    List<String> urlList = new ArrayList<>();
+    urlList.add("https://den.dev/index.xml");
+    urlList.add("https://den.dev/index.xml");
+    log.info("Feed list size {}", parser.parseRssFeedItemFromMultipleUrls(urlList).size());
   }
 }
--- src/main/java/com/aymar/rss/parser/RssFeedParser.java
+++ src/main/java/com/aymar/rss/parser/RssFeedParser.java
@@ -1,20 +1,42 @@
 package com.aymar.rss.parser;
 
 import com.aymar.rss.dto.FeedItem;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class RssFeedParser {
 
   private SAXParserFactory factory = SAXParserFactory.newInstance();
-  FeedItemHandler feedItemHandler = new FeedItemHandler();
+  ExecutorService rssParserPoll =
+      Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
   @SneakyThrows
-  public List<FeedItem> parseRssFeedItems(String url) {
+  public List<FeedItem> parseRssFeedItemFromURL(String url) {
     SAXParser saxParser = factory.newSAXParser();
+    FeedItemHandler feedItemHandler = new FeedItemHandler();
     saxParser.parse(url, feedItemHandler);
     return feedItemHandler.getFeedItemList();
+  }
+
+  @SneakyThrows
+  public List<FeedItem> parseRssFeedItemFromMultipleUrls(List<String> urlList) {
+    List<Future<List<FeedItem>>> futureList = new ArrayList<>();
+    for (String url : urlList) {
+      futureList.add(rssParserPoll.submit(() -> parseRssFeedItemFromURL(url)));
+    }
+    List<FeedItem> feedItems = new ArrayList<>();
+    for (Future<List<FeedItem>> future : futureList) {
+      feedItems.addAll(future.get());
+    }
+    rssParserPoll.shutdown();
+    return feedItems;
   }
 }
